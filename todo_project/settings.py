e0a1274b01e250c93d1c984f1ea0fcb01e8150a3
anandhu <anandhuuthaman6@gmail.com>
2024-06-04 20:24:49 +0530

    done

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*-2bc%@auyd!=s890)hjp3qqo3#0p4o3l$-%_lr&mory()*5&3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['AppuAnandhu.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
